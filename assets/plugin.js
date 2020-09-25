baf4effb3b64cf1bcbd120d6895cfd0b375b2296
Toshihiko Ichida <dogatana@gmail.com>
2020-09-25 16:48:00 +0900

    add icon configuration

--- assets/plugin.js
+++ assets/plugin.js
@@ -11,13 +11,15 @@ gitbook.events.on('page.change', function () {
     // Default config values
     let _maxTocDepth = 2;
     let _minTocSize = 2;
+    let _icon = '<i class="fa fa-bars" aria-hidden="true"></i>';
 
 
     // Read out config data
     if (options) {
         _maxTocDepth = options.maxTocDepth ? options.maxTocDepth : _maxTocDepth;
         _minTocSize = options.minTocSize ? options.minTocSize : _minTocSize;
-    };
+        _icon = options.icon ? options.icon : _icon;
+    }
 
     // Search for headers
     var headerArray = $(".book .body-inner .markdown-section :header");
@@ -78,7 +80,7 @@ gitbook.events.on('page.change', function () {
     }
 
     // Generate html for button and menu
-    let html = "<div class='page-toc-button'><i class='fa fa-bars' aria-hidden='true'></i><div class='page-toc-menu'><ul>";
+    let html = `<div class='page-toc-button'>${_icon}<div class='page-toc-menu'><ul>`;
     for (let i = 0; i < tocArray.length; i++) {
         html += "<li><a href='#" + tocArray[i].url + "'>" + tocArray[i].name + "</a></li>";
         if (tocArray[i].children.length > 0) {
